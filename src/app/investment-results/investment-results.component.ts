156f5a2178940bfcae82639024c08abe42dffc9e
abc <you@example.com>
2024-08-17 22:17:55 +0300

    first commit

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, computed, inject, input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  private investmentService = inject(InvestmentService);
+  results = computed(() => {
+    return this.investmentService.resultsData();
+  });
+}
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,31 @@
+import { Component, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  calculate = output<InvestmentInput>();
+
+  enteredInitialInvestment = signal<string>('0');
+  enteredAnnualInvestment = signal<string>('0');
+  enteredDuration = signal<string>('10');
+  enteredExpectedRetrun = signal<string>('5');
+
+  constructor(private investmentService: InvestmentService) {}
+
+  onSubmit() {
+    this.investmentService.CalculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedRetrun(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+    });
+  }
+}
